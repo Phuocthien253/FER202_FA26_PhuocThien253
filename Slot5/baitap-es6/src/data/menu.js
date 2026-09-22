b1be35ee58f67193d06cec21639b122e65d18e87
Phuocthien253 <phuocthien189@gmail.com>
2026-09-23 03:01:08 +0700

    Delete Slot5/baitap-es6 directory

--- Slot5/baitap-es6/src/data/menu.js
+++
@@ -1,7 +0,0 @@
-export const APP_NAME = 'ES6 Shop';
-export const menuItems = [
-  { label: 'Trang chủ', href: '#home' },
-  { label: 'Sản phẩm', href: '#products' },
-  { label: 'Giỏ hàng', href: '#cart' },
-  { label: 'Đăng ký', href: '#register' },
-];
